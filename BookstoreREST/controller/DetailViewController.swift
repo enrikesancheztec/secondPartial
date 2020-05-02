911f56937af459cd838146de66ce70781460d7dd
Enrique Sanchez <enrike.sanchez@tec.mx>
2020-05-02 14:19:16 -0400

    Answer 4

--- BookstoreREST/controller/DetailViewController.swift
+++ BookstoreREST/controller/DetailViewController.swift
@@ -12,6 +12,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var coverImageView: UIImageView!
     
     var delegate : BookDelegate?
     
@@ -21,6 +22,15 @@ class DetailViewController: UIViewController {
             titleLabel.text = book.title
             authorLabel.text = book.author
             descriptionTextView.text = book.description
+            
+            let coverUrl = URL(string: "https://booksappsample.herokuapp.com/books/cover/" + String(book.bookId))
+            getData(from: coverUrl!) { data, response, error in
+                guard let data = data, error == nil else { return }
+                DispatchQueue.main.async() {
+                    self.coverImageView.image = UIImage(data: data)
+                }
+            }
+            
         }
     }
 
@@ -39,5 +49,11 @@ class DetailViewController: UIViewController {
     @IBAction func deleteBook(_ sender: UIBarButtonItem) {
         delegate?.deleteBook(self)
     }
+    
+        //MARK: Functions
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
 }
 
